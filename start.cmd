fbafa5983f5cf65c861dc077acafc42b182c80d6
MiataBoy <miataboymx@gmail.com>
2023-02-16 16:36:15 +0100

    update start.cmd file

--- start.cmd
+++ start.cmd
@@ -1,1 +1,4 @@
-python assistant.py
+python C:\filepath_to_your_py_file
+REM | You should replace the above file path with the exact file path leading to the assistant.py file
+REM | This is only necessary in the occasion that you plan to have RoVert (or your preferred name) start up with your pc.
+REM | Check the documentation on the github (https://github.com/Miataboy/RoVert) if you want to know more
